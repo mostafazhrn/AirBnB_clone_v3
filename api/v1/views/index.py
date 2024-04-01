637476190bcdfc379931f69fa1554d356c3d547b
mostafazhrn <mostafazhrn@gmail.com>
2024-04-01 11:54:00 -0400

    fix_index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 """ This script shall start a Flask web application """
-from app import app
-from flask import jsonify
-from flask import Flask, jsonify
+from api.v1.views import app_views
+from flask import Flask, jsonify, Blueprint
 from flask_cors import CORS
 from models import storage
-from api.v1.views import app_views
 from models.state import State
 from os import getenv
 
 
-@app.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ This instance shall return a JSON-formatted status response """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def gt_stats():
+    """ This instance shall return a stat resp in JSON form """
+    ob = {"amenities": "Amenity", "cities": "City", "places": "Place",
+          "reviews": "Review", "states": "State", "users": "User"}
+    for cle, valu in ob.items():
+        ob[cle] = storage.count(valu)
+    return jsonify(ob)
